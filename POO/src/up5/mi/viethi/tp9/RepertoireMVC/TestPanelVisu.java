70f37575b64c562db068aba28656fae9c1f44611
melkir <melkir13@gmail.com>
2013-12-10 02:02:46 +0100

    bug...

--- POO/src/up5/mi/viethi/tp9/RepertoireMVC/TestPanelVisu.java
+++ POO/src/up5/mi/viethi/tp9/RepertoireMVC/TestPanelVisu.java
@@ -1,5 +1,17 @@
 package up5.mi.viethi.tp9.RepertoireMVC;
 
 public class TestPanelVisu {
-
+	// création d'un chat
+	Repertoire repertoire = new Repertoire();
+	// une fenetre de visualisation du chat
+	final PanelVisuRepertoire panel = new PanelVisuRepertoire(repertoire);
+	
+	RepertoireListener repertoireListener = new RepertoireListener() {
+		public void entreeAjoute(Repertoire repertoire, String nom, String tel) {
+			panel.entreeAjoute(nom, tel);
+		}
+	};
+	
+	/* Erreur : repertoire.addRepertoireListener(repertoireListener); */ 
+	
 }
